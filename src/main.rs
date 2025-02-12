c040878937fa2474c171849ca4e0ea7c63da7450
Philipp Bisson <philipp.bisson@atrigam.com>
2025-02-12 14:25:45 +0700

    refactor project structure and rename to gitlab-cli; update dependencies and modify main logic for issue retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 use clap::{Parser, Subcommand};
-use gitlab::{
-    api::{
-        self,
-        groups::issues::Issues,
-        users::{CurrentUser, UserProjects, Users},
-        Query,
-    },
-    Gitlab,
-};
+use reqwest::{Client, RequestBuilder};
 
 mod response_types;
-
-use serde::Deserialize;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -25,57 +15,33 @@ enum Commands {
     Login,
 }
 
-// The return type of a `Project`. Note that GitLab may contain more information, but you can
-// define your structure to only fetch what is needed.
-#[derive(Debug, Deserialize)]
-struct User {
-    id: u64,
-    username: String,
+fn client(url: &str, token: &str) -> RequestBuilder {
+    let client = Client::new();
+    client.get(url).bearer_auth(token)
 }
 
-#[derive(Debug, Deserialize)]
-struct UserProject {
-    name: String,
-}
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     //
     // from env
     //
     let token = std::env::var("GITLAB_TOKEN").expect("Could not find GITLAB_TOKEN in env");
-
     match &cli.command {
         Some(Commands::Login) => {
-            let client = Gitlab::new("gitlab.com", token).expect("Gitlab client");
-            let user: response_types::GitLabUser = CurrentUser::builder()
-                .build()
-                .unwrap()
-                .query(&client)
-                .unwrap();
-
-            let user_project: Vec<response_types::GitLabProject> = UserProjects::builder()
-                .user(user.id)
-                .build()
-                .unwrap()
-                .query(&client)
-                .unwrap();
-
-            let issues: Vec<response_types::GitLabIssue> = Issues::builder()
-                .assignee_id(user.id)
-                .build()
-                .unwrap()
-                .query(&client)
-                .unwrap();
-
-            // let raw_data: Vec<u8> = api::raw(&b).query(&client).unwrap();
-            // let raw = String::from_utf8(raw_data).unwrap(); // raw_data.into();
-
-            dbg!(&user);
-            dbg!(&user_project);
-            dbg!(&issues);
+            let response = client(
+                "https://gitlab.com/api/v4/issues?assignee_id=4500276",
+                &token,
+            )
+            .send()
+            .await?
+            .json::<Vec<response_types::GitLabIssue>>()
+            .await?;
+            dbg!(response);
         }
         None => (),
     }
+
+    Ok(())
 }
--- src/response_types.rs
+++ src/response_types.rs
@@ -1,5 +1,4 @@
-use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
 /// GitLab User Response
 #[derive(Debug, Serialize, Deserialize)]
